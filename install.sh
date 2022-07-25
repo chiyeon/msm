0e482f25d3df34efd931323fd0dc00d09a9b4d12
Benjamin Wong <benjiw@recogni.com>
2022-07-25 16:35:11 -0700

    updated install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,17 @@
+shc -f ./src/msm.sh
+
+if [ -f ./src/msm.sh.x ]; then
+    mv ./src/msm.sh.x ./bin/msm
+    rm ./src/msm.sh.x.c
+
+    echo "Compiled to /bin/msm/"
+
+    {       # try
+        sudo cp ./bin/msm /usr/local/bin/ &&
+        echo "Done. Try running 'msm' in the console!"
+    } || {  #catch
+        echo "Couldn't copy msm to /usr/local/bin/"
+    }
+else
+    echo Failed to compile.
+fi
